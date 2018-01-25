fbb02c5bfa77fe659ffb36b86ee710af85938202
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2018-01-25 11:42:43 +0300

    Initial Commit

---
+++ PracticalTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticalTesting
+//
+//  Created by Seyfeddin Bassarac on 25/01/2018.
+//  Copyright © 2018 ThreadCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
